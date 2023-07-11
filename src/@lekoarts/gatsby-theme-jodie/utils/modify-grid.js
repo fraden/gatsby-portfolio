cbcd6d11f50e472f040947f1c273bede17f75f6d
Dennis Frankenbach <dennisfrankenbach@gmail.com>
2023-07-12 01:16:45 +0200

    feat: update images

--- src/@lekoarts/gatsby-theme-jodie/utils/modify-grid.js
+++ src/@lekoarts/gatsby-theme-jodie/utils/modify-grid.js
@@ -1,5 +1,5 @@
 import { filterBySlug } from "@lekoarts/gatsby-theme-jodie/src/utils/resolver-templates";
 
-const modifyGrid = (data) => filterBySlug(data, ["/about", "/portrait", "/black-and-white", "/nature", "/animals", "/arts", "/cities", "/wedding"]);
+const modifyGrid = (data) => filterBySlug(data, ["/about", "/wedding", "/portrait", "/black-and-white", "/nature", "/animals", "/arts", "/cities"]);
 
 export default modifyGrid;
